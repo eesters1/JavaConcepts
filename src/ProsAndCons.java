1f892ad981e0c4969abcd1a6cb594cca4b5b4786
eesters1 <45569423+eesters1@users.noreply.github.com>
2022-11-02 21:08:45 -0500

    completed pros and cons

--- src/ProsAndCons.java
+++ src/ProsAndCons.java
@@ -59,19 +59,39 @@ public class ProsAndCons {
     public static void pros() {
         //System.out.println("This section is for pros");
         System.out.println("\n***** Pros Elaborated *****\n");
-        System.out.println("1. Simple - ");
-        System.out.println("2. Object-Oriented Programming");
-        System.out.println("3. Secure Language");
-        System.out.println("4. Cheap and Economical to Maintain");
-        System.out.println("5. Platform-Independent");
-        System.out.println("6. High-Level Programming Language");
-        System.out.println("7. Supports Portability Feature");
-        System.out.println("8. Provides Automatic Garbage Collection");
-        System.out.println("9. Supports Multithreading");
-        System.out.println("10. Stable");
-        System.out.println("11. Distributed Language");
-        System.out.println("12. Efficient Memory Allocation System");
-        System.out.println("13. Massive Community Support");
+        System.out.println("1. Simple" +
+                "\nJava is easy to learn and understand. It removes all complex features  of C and C++ like pointers, structures, unions, and " +
+                "\nmakes the implementation of codes easier.");
+        System.out.println("\n2. Object-Oriented Programming" +
+                "\nOOP helps maintain large code by breaking it into smaller fragments.");
+        System.out.println("\n3. Secure Language" +
+                "\nJava uses OOP concepts like encapsulation, abstraction, and inheritance which increases security and prevents" +
+                "\nunauthorized access to users. Unlike C and C++ which use pointers that allow access to memory location. ");
+        System.out.println("\n4. Cheap and Economical to Maintain" +
+                "\nDue to its simple build, java is cheap and easy to develop and maintain. The fact that it is platform-independent reduces cost greatly.");
+        System.out.println("\n5. Platform-Independent" +
+                "\nJava can be run on any systems that has Java in it. It does not depend on OS or hardware.");
+        System.out.println("\n6. High-Level Programming Language" +
+                "\nJava is written in high-level (or human) language. It has an interpreter which interprets the code to machine level.");
+        System.out.println("\n7. Supports Portability Feature" +
+                "\nJava is highly portable because it is platform independent as it does not require special hardware to run.");
+        System.out.println("\n8. Provides Automatic Garbage Collection" +
+                "\nIn Java, the JVM automatically manages the memory. So, whenever there is an object that doesn't refer to any class and needs" +
+                "\nto be de-referenced, the JVM automatically removes them from the program.");
+        System.out.println("\n9. Supports Multithreading" +
+                "\nA thread is the smallest possible unit of a process. To gain max CPU utilization, multithreading is a key component." +
+                "\nOn Java, we can run more that one thread at a time. They share a common memory to increase the efficiency and performance" +
+                "\nof the application. Threads run independently of one another.");
+        System.out.println("\n10. Stable" +
+                "\nJava receives regular updates to remove bugs.");
+        System.out.println("\n11. Distributed Language" +
+                "\nJava has the mechanism for sharing data and programs among multiple computers. " +
+                "\nIt also supports RMI (Remote Method Invocation) and socket programming.");
+        System.out.println("\n12. Efficient Memory Allocation System" +
+                "\nJava divides memory into 2 parts: heap and stack. The JVM allocates memory from either of the two parts depending on the requirement.");
+        System.out.println("\n13. Massive Community Support" +
+                "\nJava has been able to build a community of developers larger than any other programming language in the world." +
+                "\nBecause of this, almost all Java queries have been resolved in StackOverflow.");
 
         ProsAndCons.elaborate();
     }
