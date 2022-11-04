d2ebb58ba40263b201d4ebe492a8e96580797e6b
eesters1 <45569423+eesters1@users.noreply.github.com>
2022-11-04 17:00:04 -0500

    Built fundamentals base code

--- src/Fundamentals.java
+++ src/Fundamentals.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
 public class Fundamentals {
     public static void main() {
         System.out.println("\nThis is the Java Fundamentals page!"); //Test to ensure .java file is working
 
+        Fundamentals.fundamentalMenu();
+        Fundamentals.choose();
+    }
+    public static void fundamentalMenu() {
+        System.out.println("");
+    }
+    public static void choose(){
+        int choice;
+        Scanner sc = new Scanner(System.in);
+
+        choice = sc.nextInt(); //Store selected menu choice
+
+        switch(choice) {         //Direct user to proper page or submenu
+            case 1:
+                break;
+            default:
+        }
     }
 }
