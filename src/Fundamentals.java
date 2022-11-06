54e550fb2838bb6efe7703533aa15003be60b455
eesters1 <45569423+eesters1@users.noreply.github.com>
2022-11-06 17:04:07 -0600

    finish fundamentals

--- src/Fundamentals.java
+++ src/Fundamentals.java
@@ -14,8 +14,8 @@ public class Fundamentals {
         System.out.println("1. Data Types");
         System.out.println("2. Operators");
         System.out.println("3. Loops");
-        System.out.println("4. Methods");
-        System.out.println("5. Input");
+        System.out.println("4. Methods/Functions");
+        System.out.println("5. Packages");
         System.out.println("6. Files");
 
         choice = sc.nextInt(); //Store selected menu choice
@@ -29,7 +29,7 @@ public class Fundamentals {
                 break;
             case 4: Fundamentals.methods();
                 break;
-            case 5: Fundamentals.input();
+            case 5: Fundamentals.packages();
                 break;
             case 6: Fundamentals.files();
                 break;
@@ -156,17 +156,54 @@ public class Fundamentals {
         Fundamentals.repeat();
     }
     public static void methods() {
-        System.out.println("\n***** Methods *****\n");
+        System.out.println("\n***** Methods/Functions *****\n");
+        System.out.println(" - A method/function is a block of code which only runs when it is called.\n" +
+                " - A method must be declared within a class. (ex. public class Main { static void myMethod() {} })\n" +
+                " - A method can be called using the method's name followed by two parentheses (ex. myMethod();)\n" +
+                " - Information can be passed to methods as parameters. (ex. static void myMethod(String fname) {})\n" +
+                " - If you want the method to return a value, you can use a primitive data type instead of void and \n" +
+                "   use the return keyword inside the method. (ex. static int myMethod(int x) {return 5 + x;})\n" +
+                " - The result of a method can also be stored in a variable. (ex. int z = myMethod(5, 3));\n" +
+                "   \n*** Important Terms ***\n" +
+                " - Static - the method belongs to the Main class and is not an object of the main class\n" +
+                " - Void - this method does not have a return value.\n" +
+                " - Return - keyword to determine what value will be returned from the method");
 
         Fundamentals.repeat();
     }
-    public static void input() {
-        System.out.println("\n***** Input *****\n");
+    public static void packages() {
+        System.out.println("\n***** Packages *****\n");
+        System.out.println(" - The full list of built-in Java packages can be found at: https://docs.oracle.com/javase/8/docs/api/\n");
+        System.out.println(" - You can also declare your own packages.\n" +
+                "      - Create a package: package mypack;\n" +
+                "      - Import a single class: import package.name.Class;\n" +
+                "      - Import the whole package: import package.name.*;\n" +
+                " \n*** Common Java Packages ***\n" +
+                " - java.util.Scanner - used to get user input\n" +
+                " - java.io - contains classed for supporting file input/output operations\n" +
+                " - java.util - contains utility classes which implement data structures linke linked list, dictionary, data/time operations, etc.\n" +
+                " - java.applet - contains classes for creating applets\n" +
+                " - java.awt - contains classes for implementing the components for graphical user interfaces (buttons, menus, etc.)\n" +
+                " - java.net - contains classes for supporting networking operations");
 
         Fundamentals.repeat();
     }
     public static void files() {
         System.out.println("\n***** Files *****\n");
+        System.out.println(" - To handle files you must use import java.io.File;\n" +
+                " - To use the file class, create an object of the class and specify \n" +
+                "   the filename or directory name (ex. File myObj = new File('filename.txt');\n" +
+                "\n*** Common Methods ***\n" +
+                " - canRead() - boolean, Tests whether the file is readable or not\n" +
+                " - canWrite() - boolean, Tests whether the file is writable or not\n" +
+                " - createNewFile() - boolean, Creates an empty file\n" +
+                " - delete() - boolean, Deletes a file\n" +
+                " - exists() - boolean, Tests whether the file exists\n" +
+                " - getName() - string, Returns the name of the file\n" +
+                " - getAbsolutePath() - string, Returns the absolute pathname of the file\n" +
+                " - length() - long, Returns the size of the file in bytes\n" +
+                " - list() - String[], Returns an array of the files in the directory\n" +
+                " - mkdir() - boolean, Creates a directory\n");
 
         Fundamentals.repeat();
     }
